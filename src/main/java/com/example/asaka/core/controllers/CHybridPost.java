487b29caff8150d1503dd04f55f4595aa70077b1
Saidazim <saidazimabduhamidov@gmail.com>
2023-11-03 09:57:11 +0500

    Added Hybrid Post APIs and "Test API" for working with REST API in database

---
+++ src/main/java/com/example/asaka/core/controllers/CHybridPost.java
@@ -0,0 +1,60 @@
+package com.example.asaka.core.controllers;
+
+/*
+{
+  Author: Saidazim,
+  Cr_on: 30.10.2023,
+  Purpose: Rest API for working with Hybrid Post
+}
+*/
+
+import com.example.asaka.core.services.SHybridPost;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/hybrid")
+public class CHybridPost {
+
+  @Autowired
+  SHybridPost sHybridPost;
+
+  @RequestMapping(value = "/auth", produces = "application/json")
+  public String hybridPostAuth() {
+    return sHybridPost.auth();
+  }
+
+  @RequestMapping(value = "/get-http-token", produces = "application/json")
+  public String getHttpAuthToken(@RequestBody String params) {
+    return sHybridPost.getHttpToken(params);
+  }
+
+  @RequestMapping(value = "/send-http-request", produces = "application/json")
+  public String sendHttpRequest(@RequestBody String params) {
+    return sHybridPost.sendHttpRequest(params);
+  }
+
+  @RequestMapping(value = "/create-mail", produces = "application/json")
+  public String createMail(@RequestBody String params) {
+    JSONObject result = new JSONObject(sHybridPost.checkCreatingMail(params));
+    if (result.getBoolean("success")) {
+      return sHybridPost.createMail(params);
+    }
+    return result.toString();
+  }
+
+  @RequestMapping(value = "/send-mail", produces = "application/json")
+  public String sendMail(@RequestBody String params) {
+    String result = sHybridPost.checkSendingMail(params);
+    if (result != null) {
+      return sHybridPost.sendMail(params);
+    }
+    return null;
+  }
+}
+
--- src/main/java/com/example/asaka/util/JbUtil.java
+++ src/main/java/com/example/asaka/util/JbUtil.java
@@ -1,12 +1,14 @@
 package com.example.asaka.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.FileCopyUtils;
-//import org.springframework.util.ResourceUtils;
 
-//import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -14,45 +16,52 @@ import java.nio.charset.StandardCharsets;
 
 public class JbUtil {
 
-    public static String nvl(String str) {
-        return str == null ? "" : str;
-    }
+  public static String nvl(String str) {
+    return str == null ? "" : str;
+  }
 
-    public static String nvl(String str, String defaultValue) {
-        String returnStr = defaultValue;
-        if (str != null && !str.equals(""))
-            returnStr = str;
-        return returnStr;
-    }
+  public static String nvl(String str, String defaultValue) {
+    String returnStr = defaultValue;
+    if (str != null && !str.equals(""))
+      returnStr = str;
+    return returnStr;
+  }
 
-    public static String getSeparatorForCurrentOS() {
-        return isWindows() ? "\\" : "//";
-    }
+  public static String getSeparatorForCurrentOS() {
+    return isWindows() ? "\\" : "//";
+  }
 
-    public static boolean isWindows() {
-        String os = System.getProperty("os.name").toLowerCase();
-        //windows
-        return (os.indexOf("win") >= 0);
-    }
+  public static boolean isWindows() {
+    String os = System.getProperty("os.name").toLowerCase();
+    //windows
+    return (os.indexOf("win") >= 0);
+  }
 
-    public static JSONObject getJson(String path) {
+  public static JSONObject getJson(String path) {
 //		File file = null;
-        ClassPathResource cpr = new ClassPathResource(path + ".json");
-        String data = "";
-        try {
+    ClassPathResource cpr = new ClassPathResource(path + ".json");
+    String data = "";
+    try {
 //			file = ResourceUtils.getFile(path + ".json");
-            byte[] bdata = FileCopyUtils.copyToByteArray(cpr.getInputStream());
-            data = new String(bdata, StandardCharsets.UTF_8);
+      byte[] bdata = FileCopyUtils.copyToByteArray(cpr.getInputStream());
+      data = new String(bdata, StandardCharsets.UTF_8);
 //			String content = new String(Files.readAllBytes(file.toPath()));
-            JSONObject json = new JSONObject(data);
-            return json;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
+      JSONObject json = new JSONObject(data);
+      return json;
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (JSONException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
+
+  public static String jsonStringMapper(String jsonString) throws JsonProcessingException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode jsonNode = objectMapper.readTree(jsonString);
+    ObjectNode object = (ObjectNode) jsonNode;
+    return objectMapper.writeValueAsString(object);
+  }
 }
